0524eece4688684de28f98fbfd8873e6ae17a087
parvathi982005 <parvathitutarapu@gmail.com>
2026-06-17 14:32:09 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.use("/api/auth", require("./routes/auth"));
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
